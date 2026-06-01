87ca1d1feb9820723b16c8433acfe1a80c6162e4
pnkolom <mvogopierre129@gmail.com>
2026-06-01 10:33:32 +0200

    new commit

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
